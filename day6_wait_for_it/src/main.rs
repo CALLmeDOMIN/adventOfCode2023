b256aad4d1b2906afa7f5d988ae5b3ffa0617274
Dominik Sieroń <dominiksieron13@gmail.com>
2023-12-06 21:10:47 +0100

    Add: day6 part2

--- day6_wait_for_it/src/main.rs
+++ day6_wait_for_it/src/main.rs
@@ -11,6 +11,9 @@ fn main() {
     let time: Vec<&str> = data[0][13..].split("     ").collect();
     let distance: Vec<&str> = data[1][12..].split("   ").collect();
 
+    let mut new_time_string: String = String::new();
+    let mut new_distance_string: String = String::new();
+
     let mut ways_to_beat_vec: Vec<i32> = Vec::new();
 
     for i in 0..time.len() {
@@ -18,6 +21,10 @@ fn main() {
 
         let race_time: i32 = time[i].parse::<i32>().unwrap();
         let distance_to_beat: i32 = distance[i].parse::<i32>().unwrap();
+
+        //part2
+        new_time_string += time[i];
+        new_distance_string += distance[i];
 
         for j in 1..race_time {
             let distance_travelled: i32 = (race_time - j) * j;
@@ -35,5 +42,19 @@ fn main() {
         multiply *= integer;
     }
 
-    println!("{:?}", multiply);
+    println!("part1: {}", multiply);
+
+    //part 2
+    let new_time: u64 = new_time_string.parse::<u64>().unwrap();
+    let new_distance: u64 = new_distance_string.parse::<u64>().unwrap();
+
+    let mut new_ways_to_beat: u64 = 0;
+
+    for k in 1..new_time {
+        if ((new_time - k) * k) > new_distance {
+            new_ways_to_beat += 1;
+        }
+    }
+
+    println!("part2: {}", new_ways_to_beat);
 }
