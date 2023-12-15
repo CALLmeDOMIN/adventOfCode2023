6628c2ac42addf035226960d2554f0873eb70079
Dominik Sieroń <dominiksieron13@gmail.com>
2023-12-16 00:29:56 +0100

    Add: Day15 Part2 almost done

---
+++ day15_lens_library/part2/src/main.rs
@@ -0,0 +1,97 @@
+use std::collections::HashMap;
+
+fn hash_letters(letters: String) -> i32 {
+    let mut current_value: i32 = 0;
+    let ascii_values: Vec<u8> = letters.chars().map(|c| c as u8).collect();
+    for val in ascii_values {
+        current_value += val as i32;
+        current_value *= 17;
+        current_value %= 256;
+    }
+
+    current_value
+}
+
+fn get_id(txt: String) -> String {
+    txt[..txt.len() - 2].to_string()
+}
+
+fn get_val(txt: String) -> String {
+    txt[txt.len() - 1..].to_string()
+}
+
+fn hashy_hashy(input: Vec<String>) -> HashMap<i32, Vec<String>> {
+    let mut boxes: HashMap<i32, Vec<String>> = Default::default();
+    for step in input {
+        let id: String = get_id(step.clone());
+        let val: String = get_val(step.clone());
+        let key: i32 = hash_letters(id.clone());
+
+        if step.ends_with("-") {
+            if let Some(entry) = boxes.get_mut(&key) {
+                entry.retain(|e| get_id(e.clone()) != id);
+            }
+        } else {
+            let new_entry: String = id.clone() + " " + val.clone().as_str();
+
+            if let Some(entry) = boxes.get_mut(&key) {
+                let mut swapped: bool = false;
+                for e in entry.iter_mut() {
+                    if get_id(e.clone()) == id {
+                        *e = new_entry.clone();
+                        swapped = true;
+                        break;
+                    }
+                }
+                if !swapped {
+                    entry.push(new_entry);
+                }
+            } else {
+                boxes.insert(key, vec![new_entry]);
+            }
+        }
+    }
+    boxes
+}
+
+fn the_hashy_counter(boxes: HashMap<i32, Vec<String>>) -> i32 {
+    let mut sum: i32 = 0;
+
+    for (key, value) in boxes.iter() {
+        if value.len() == 0 {
+            continue;
+        }
+
+        let box_number: i32 = key + 1;
+        for (i, val) in value.iter().enumerate() {
+            let slot_number: i32 = i as i32 + 1;
+            let focal_len: i32 = get_val(val.clone()).parse::<i32>().unwrap();
+            sum += box_number * slot_number * focal_len;
+        }
+    }
+
+    sum
+}
+
+fn main() {
+    let file = std::fs::read_to_string("input.txt").expect("File not found!");
+    let input = file.lines().collect::<Vec<_>>().join(",");
+
+    let boxes: HashMap<i32, Vec<String>> =
+        hashy_hashy(input.split(',').map(|s| s.to_string()).collect());
+
+    let total_sum: i32 = the_hashy_counter(boxes.clone());
+
+    println!("{}", total_sum);
+
+    // let mut boxes: Vec<(&i32, &Vec<String>)> = boxes.iter().collect();
+    // boxes.sort_by(|a, b| a.0.cmp(b.0));
+
+    // for boxy in boxes.iter() {
+    //     println!("{}: {:?}", boxy.0, boxy.1);
+    // }
+}
+
+// 28231 too low
+// 228181 too low
+// 404846 too high
